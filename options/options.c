f89c270cb2de4f72de3f7ba733e7059d24de2149
Sergey Malyshev <in1t@ya.ru>
2020-07-31 19:05:51 +0300

    options

--- options/options.c
+++ options/options.c
@@ -1,53 +1,75 @@
 #include "unistd.h"
 
-int		options(char *str, int *aopt)
+void	ft_putchar(char ch)
 {
-	if (*str != '-')
-		return (1);
-	str++;
-	while (*str)
+	write(1, &ch, 1);
+}
+
+void	ft_putstr(const char *str)
+{
+	if (str)
 	{
-		if (*str == 'h')
+		while (*str)
 		{
-			write(1, "options: abcdefghijklmnopqrstuvwxyz\n", 36);
-			return (0);
+			ft_putchar(*str);
+			str++;
 		}
-		if (*str < 'a' || *str > 'z')
-		{
-			write(1, "Invalid Option\n", 15);
-			return (0);
-		}
-		*aopt = *aopt | (1 << (*str - 'a'));
-		str++;
 	}
-	return (1);
+}
+
+void	ft_putopt(int opt)
+{
+	int		i;
+
+	i = 32;
+	while (i > 0)
+	{
+		i--;
+		ft_putchar(opt & (1 << i) ? '1' : '0');
+		if (i > 0 && i % 8 == 0)
+			ft_putchar(' ');
+	}
+}
+
+int		options(char *s, int *aopt)
+{
+	if (*s == '-')
+	{
+		s++;
+		while (*s)
+		{
+			if (*s < 'a' || *s > 'z')
+				return (-1);
+			if (*s == 'h')
+				return (0);
+			*aopt |= (1 << (*s - 'a'));
+			s++;
+		}
+	}	
+	return (1);	
 }
 
 int		main(int argc, char *argv[])
 {
 	int	res;
+	int	opt;
 	int	index;
 
-	if (argc == 1)
-	{
-		write(1, "options: abcdefghijklmnopqrstuvwxyz\n", 36);
-		return (0);
-	}
 	res = 0;
+	opt = 0;
 	index = 1;
 	while (index < argc)
 	{
-		if (!options(argv[index], &res))
-			return (0);
+		if ((res = options(argv[index], &opt)) != 1)
+			break ;
 		index++;
 	}
-	index = 32;
-	while (index > 0)
-	{
-		index--;
-		write(1, res & (1 << index) ? "1" : "0", 1);
-		if (index && index % 8 == 0)
-			write(1, " ", 1);
-	}
-	write(1, "\n", 1);
+	if (res == -1)
+		ft_putstr("Invalid Option");
+	else if (res == 0)
+		ft_putstr("options: abcdefghijklmnopqrstuvwxyz");
+	else
+		ft_putopt(opt);
+	ft_putchar('\n');
+	return (0);
 }
